0fe95f716d3cacb54916287f42c94de6902ff241
qx00qx <qx00qx@rambler.ru>
2024-11-03 19:53:13 +0300

    test

---
+++ cypress/e2e/specForm.cy.js
@@ -0,0 +1,40 @@
+describe('Базовые сценарии отправки формы', () => {
+  beforeEach(() => {
+    cy.visit('https://forma-obratnoy-svyazi-dlya-sayta-614458.testograf.ru');
+  })
+
+  it('Успешная отправка формы', () => {
+    cy.get('input[placeholder=""]').eq(0) 
+    .type('Иван');
+
+    cy.get('input[placeholder=""]').eq(1)
+    .type('ivan.ivanov@example.com')
+
+    cy.get('input[placeholder=""]').eq(2)
+    .type('+79002003050')
+
+    cy.get('input[placeholder=""]').eq(3)
+    .click()
+    cy.contains('Заказ').click()
+
+    cy.get('textarea').type('Это тестовое сообщение.');
+
+    cy.get('button').click();
+
+    cy.contains('Благодарим за обращение!').should('be.visible');
+  })
+  
+  it('Некорректная отправка формы', () => {
+
+    cy.get('input[placeholder=""]').eq(1)
+    .type('ivan.ivanov@example.com')
+
+    cy.get('input[placeholder=""]').eq(2)
+    .type('+79002003050')
+
+    cy.get('textarea').type('Это тестовое сообщение.');
+    cy.get('button').click();
+    
+    cy.contains('Вопрос является обязательным').should('be.visible')
+  })
+})
